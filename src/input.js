b22c10c34434d2b8bf23efb7fda7f4f2a366713c
rekha_verma <rekhavermakuk56@gmail.com>
2019-02-15 11:38:11 +0530

    Initial setup of react and webpack

--- src/input.js
+++
@@ -1,3 +0,0 @@
-export default function giveMeSomeInput() {
-    console.log('I get called from input.js!');
-  }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,41 @@
- const merge = require('webpack-merge');
- const common = require('./webpack.common.js');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist'
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+     ]
    }
- });
+};
